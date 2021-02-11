3877170957ab44b803f21f330256a2ef561e019c
Sebastian Parra <sebastian.parra@externos.bci.cl>
2021-02-11 14:48:29 -0300

    fix ingreso masivo, se agrea mensaje de error al usuario en caso de no encontrar comuna

--- src/app/modules/paginas/formulario-encomienda-masivo/formulario-encomienda-masivo.component.ts
+++ src/app/modules/paginas/formulario-encomienda-masivo/formulario-encomienda-masivo.component.ts
@@ -8,10 +8,22 @@ import { EditarEncomiendaComponent } from './editar-encomienda/editar-encomienda
 import { HttpClient } from '@angular/common/http';
 import { PuntoRetiroComponent } from '../../../util/modals/punto-retiro/punto-retiro.component';
 import { ClienteService } from 'src/app/services/cliente/cliente.service';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import { Router } from '@angular/router';
-import { faTrashAlt, faPlusSquare, faSave, faMinusSquare, faTrash, faFileExcel } from '@fortawesome/free-solid-svg-icons';
+import {
+  faTrashAlt,
+  faPlusSquare,
+  faSave,
+  faMinusSquare,
+  faTrash,
+  faFileExcel,
+} from '@fortawesome/free-solid-svg-icons';
 
 import Swal from 'sweetalert2';
 import { ResumenEncomiendaComponent } from 'src/app/util/modals/resumen-encomienda/resumen-encomienda.component';
@@ -19,10 +31,9 @@ import { ResumenEncomiendaComponent } from 'src/app/util/modals/resumen-encomien
 @Component({
   selector: 'app-formulario-encomienda-masivo',
   templateUrl: './formulario-encomienda-masivo.component.html',
-  styleUrls: ['./formulario-encomienda-masivo.component.scss']
+  styleUrls: ['./formulario-encomienda-masivo.component.scss'],
 })
 export class FormularioEncomiendaMasivoComponent implements OnInit {
-
   accionFila = ['edit'];
   seleccionFila = false;
   filasSeleccionadas = [];
@@ -35,7 +46,7 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
     direccion: '',
     comuna: { id: '', name: '', zona: '' },
     telefono: '',
-    referencia: ''
+    referencia: '',
   };
   delIcon = faMinusSquare;
   faPlusSquare = faPlusSquare;
@@ -49,14 +60,23 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
   total = 0;
   arrayCostoFinal = [];
 
-
   estadoEliminando = false;
   btnAgregar = false;
   btnEliminar = false;
 
   dataSource;
   selection;
-  displayedColumns: string[] = ['select', 'contacto', 'direccion', 'telefono', 'referencia', 'descripcion', 'dimencion', 'peso', 'accion'];
+  displayedColumns: string[] = [
+    'select',
+    'contacto',
+    'direccion',
+    'telefono',
+    'referencia',
+    'descripcion',
+    'dimencion',
+    'peso',
+    'accion',
+  ];
   haveEpecial = false;
 
   constructor(
@@ -94,26 +114,30 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
   }
 
   async getAllComuna() {
-    this.comunas = await this.cliente.callServices({ servicio: 'getComunaByProvincia' })
-      .then(v => {
-        return v.map(e => {
+    this.comunas = await this.cliente
+      .callServices({ servicio: 'getComunaByProvincia' })
+      .then((v) => {
+        return v.map((e) => {
           return {
             id: e.id_com,
             zona: e.id_zon,
-            name: e.nombre
+            name: e.nombre,
           };
         });
       })
-      .catch(e => console.log(e));
+      .catch((e) => console.log(e));
     this.getTiendasByUsuario();
   }
 
   async getTiendasByUsuario() {
     this.ptoRetiro = await this.cliente
-      .callServices({ servicio: 'getTiendasByUsuarios', body: { id: this.usuarioService.getAwknSession().id } })
-      .then(v => {
-        this.isRegistrado = ((v.length === 0) ? false : true);
-        return v.map(e => {
+      .callServices({
+        servicio: 'getTiendasByUsuarios',
+        body: { id: this.usuarioService.getAwknSession().id },
+      })
+      .then((v) => {
+        this.isRegistrado = v.length === 0 ? false : true;
+        return v.map((e) => {
           return {
             id: e.id_dir,
             name: e.nombre,
@@ -121,22 +145,23 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
             comuna: this.comunas.find((valor: any) => valor.id === e.id_com),
             direccion: e.direccion,
             telefono: e.telefono,
-            referencia: e.referencia
+            referencia: e.referencia,
           };
         });
       })
-      .catch(e => console.log(e));
+      .catch((e) => console.log(e));
     if (this.ptoRetiro.length === 0) {
       Swal.fire({
         title: 'No tiene ninguna tienda asignada',
-        text: 'Debes tener al menos UNA tienda asignada para encomiendas masivas, ¿deseas agregar una?',
+        text:
+          'Debes tener al menos UNA tienda asignada para encomiendas masivas, ¿deseas agregar una?',
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
         cancelButtonText: 'No por ahora',
         confirmButtonText: 'Sí, crearé una!',
-        heightAuto: false
+        heightAuto: false,
       }).then((result) => {
         if (result.value) {
           this.agregarPuntoRetiro();
@@ -153,7 +178,7 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
 
   cargarExcel() {
     const modalUpload = this.dialog.open(UploadComponent, {});
-    modalUpload.afterClosed().subscribe(result => {
+    modalUpload.afterClosed().subscribe((result) => {
       const excel = result[0];
       this.procesarExcel(excel);
     });
@@ -171,13 +196,13 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
       comuna: select.comuna,
       direccion: select.direccion,
       telefono: select.telefono,
-      referencia: select.referencia
+      referencia: select.referencia,
     };
   }
 
   cambioTienda(event) {
     const select = event.value;
-    this.setShop(this.ptoRetiro.find(v => v.id === select));
+    this.setShop(this.ptoRetiro.find((v) => v.id === select));
     this.valorizar(this.dataExcel);
   }
 
@@ -187,21 +212,23 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
         isNew: true,
         idComunaRetiro: this.selecShop.comuna.id,
         comunas: this.comunas,
-        fila: []
+        fila: [],
       },
       width: '700px',
-      panelClass: ['filtro-container', 'filtro-content']
+      panelClass: ['filtro-container', 'filtro-content'],
     });
-    modalFiltro.afterClosed().subscribe(result => {
+    modalFiltro.afterClosed().subscribe((result) => {
       if (result && result.resp !== 'cancelar') {
         const newArray = [];
-        this.dataExcel.forEach(v => {
+        this.dataExcel.forEach((v) => {
           newArray.push(v);
         });
-        result.id = Math.max(...newArray.map(v => v.id));
+        result.id = Math.max(...newArray.map((v) => v.id));
         newArray.push(result);
         this.dataExcel = newArray;
-        this.totalValorizado = this.dataExcel.map(v => v.Valor).reduce((a, b) => a + b);
+        this.totalValorizado = this.dataExcel
+          .map((v) => v.Valor)
+          .reduce((a, b) => a + b);
       }
     });
   }
@@ -212,15 +239,15 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
         isNew: false,
         idComunaRetiro: this.selecShop.comuna.id,
         comunas: this.comunas,
-        fila
+        fila,
       },
       width: '700px',
-      panelClass: ['filtro-container', 'filtro-content']
+      panelClass: ['filtro-container', 'filtro-content'],
     });
-    modalFiltro.afterClosed().subscribe(result => {
+    modalFiltro.afterClosed().subscribe((result) => {
       if (result && result.resp !== 'cancelar') {
         const newArray = [];
-        this.dataExcel.forEach(v => {
+        this.dataExcel.forEach((v) => {
           if (v.id === result.id) {
             newArray.push(result);
           } else {
@@ -228,7 +255,9 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
           }
         });
         this.dataExcel = newArray;
-        this.totalValorizado = this.dataExcel.map(v => v.Valor).reduce((a, b) => a + b);
+        this.totalValorizado = this.dataExcel
+          .map((v) => v.Valor)
+          .reduce((a, b) => a + b);
       }
     });
   }
@@ -245,7 +274,6 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
     }
   }
 
-
   setEstadoInicial() {
     this.estadoEliminando = false;
     this.btnAgregar = false;
@@ -253,7 +281,6 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
     this.accionFila = ['edit'];
     this.delIcon = faMinusSquare;
   }
-
 
   eliminarDetalle() {
     this.descargando = 0;
@@ -263,13 +290,15 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
       return;
     }
     const newArray = [];
-    this.dataExcel.forEach(v => {
-      if (!this.filasSeleccionadas.map(e => e.id).includes(v.id)) {
+    this.dataExcel.forEach((v) => {
+      if (!this.filasSeleccionadas.map((e) => e.id).includes(v.id)) {
         newArray.push(v);
       }
     });
     this.dataExcel = newArray;
-    this.totalValorizado = this.dataExcel.map(v => v.Valor).reduce((a, b) => a + b);
+    this.totalValorizado = this.dataExcel
+      .map((v) => v.Valor)
+      .reduce((a, b) => a + b);
     this.setEstadoInicial();
   }
 
@@ -290,21 +319,45 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
     this.valorizar(wb.data);
   }
 
+  removeAccents(str) {
+    return str
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .trim()
+      .toLocaleLowerCase();
+  }
+
   valorizar(data) {
     const auxArray = [];
     this.total = data.length;
     data.forEach((e, i) => {
+      const comunaHasta = this.comunas.find(
+        (v) => this.removeAccents(v.name) === this.removeAccents(e.Comuna)
+      );
+      if (!comunaHasta) {
+        Swal.fire({
+          title: `Comuna "${e.Comuna.toUpperCase()}" no existe dentro del listado disponible de COMUNAS.`,
+          html: `Corrija el documento con el listado que aparece como lista desplegable de la celda en la columna <u>COMUNA</u>.</br><b>- Fila numero: ${i + 3} -</b>`,
+          icon: 'warning',
+          showCancelButton: false,
+          confirmButtonColor: '#3085d6',
+          confirmButtonText: 'Ok, lo haré!',
+          heightAuto: false,
+        })
+        return;
+      }
+
       const cuerpo = {
         servicio: 'postTarifador',
         body: {
           comuna_desde: this.selecShop.comuna.id,
-          comuna_hasta: this.comunas.find(v => v.name.trim() === e.Comuna.trim()).id,
-          peso: e.Peso
-        }
+          comuna_hasta: comunaHasta.id,
+          peso: e.Peso,
+        },
       };
       this.cliente
         .callServices(cuerpo)
-        .then(resp => {
+        .then((resp) => {
           const row = {
             id: i,
             Contacto: e.Contacto,
@@ -317,13 +370,13 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
             Descripcion: e.Descripcion,
             Telefono: e.Telefono,
             Valor: resp.costo_final,
-            envio_especial: resp.envio_especial
+            envio_especial: resp.envio_especial,
           };
           this.descargando++;
           auxArray.push(row);
           this.validarDescargaDatos(auxArray);
         })
-        .catch(err => {
+        .catch((err) => {
           const row = {
             id: i,
             Contacto: e.Contacto,
@@ -336,7 +389,7 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
             Descripcion: e.Descripcion,
             Telefono: e.Telefono,
             Valor: e.costo_final,
-            envio_especial: e.envio_especial
+            envio_especial: e.envio_especial,
           };
           this.descargando++;
           auxArray.push(row);
@@ -349,21 +402,22 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
     if (this.descargando === this.total) {
       this.descargando = 0;
       this.dataExcel = auxArray;
-      this.totalValorizado = this.dataExcel.map(v => v.Valor).reduce((a, b) => a + b);
+      this.totalValorizado = this.dataExcel
+        .map((v) => v.Valor)
+        .reduce((a, b) => a + b);
       // this.cargando = false;
     } else {
       // this.cargando = true;
     }
   }
 
-
   agregarPuntoRetiro() {
     const modalFiltro = this.dialog.open(PuntoRetiroComponent, {
       data: {},
       width: '700px',
-      panelClass: ['filtro-container', 'filtro-content']
+      panelClass: ['filtro-container', 'filtro-content'],
     });
-    modalFiltro.afterClosed().subscribe(e => {
+    modalFiltro.afterClosed().subscribe((e) => {
       if ((!e || e.resp === 'cancelar') && this.ptoRetiro.length === 0) {
         this.router.navigate(['encomienda', 'vista', 'unica']);
       } else {
@@ -374,10 +428,12 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
           direccion: e.direccion,
           comuna: this.comunas.find((valor: any) => valor.id === e.id_com),
           telefono: e.telefono,
-          referencia: e.referencia
+          referencia: e.referencia,
         };
         this.ptoRetiro.push(pto);
-        this.ptoRetiroControl.setValue(this.ptoRetiro[this.ptoRetiro.length - 1].id);
+        this.ptoRetiroControl.setValue(
+          this.ptoRetiro[this.ptoRetiro.length - 1].id
+        );
         this.setShop(this.ptoRetiro[this.ptoRetiro.length - 1]);
       }
     });
@@ -400,14 +456,16 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
       km_recorrido: 0,
       valor_total: e.Valor,
       id_dir_rem: this.selecShop.idDireccion,
-      id_com_dest: this.comunas.find(v => v.name.trim() === e.Comuna.trim()).id,
+      id_com_dest: this.comunas.find((v) => v.name.trim() === e.Comuna.trim())
+        .id,
       id_usu: null,
-      id_masivo: ((id_masivo) ? id_masivo : null),
+      id_masivo: id_masivo ? id_masivo : null,
       encomienda_masiva: true,
-      envio_especial: e.envio_especial
+      envio_especial: e.envio_especial,
     };
     return {
-      servicio: 'postSetEncomienda', body
+      servicio: 'postSetEncomienda',
+      body,
     };
   }
 
@@ -417,15 +475,18 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
 
   async gestionarPedidos() {
     this.gestionTotales = this.dataExcel.length;
-    const cabecera: any = await this.getFirstRequest().then(v => v);
+    const cabecera: any = await this.getFirstRequest().then((v) => v);
     for (let index = 1; index < this.gestionTotales; index++) {
-      const resquest = this.createRequest(this.dataExcel[index], cabecera.id_masivo);
+      const resquest = this.createRequest(
+        this.dataExcel[index],
+        cabecera.id_masivo
+      );
       this.cliente
         .callServices(resquest)
-        .then(resp => {
+        .then((resp) => {
           this.validarGestionPedidos(resp);
         })
-        .catch(err => {
+        .catch((err) => {
           console.log(err);
         });
     }
@@ -436,11 +497,11 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
       const resquest = this.createRequest(this.dataExcel[0]);
       this.cliente
         .callServices(resquest)
-        .then(resp => {
+        .then((resp) => {
           this.validarGestionPedidos(resp);
           resolve(resp);
         })
-        .catch(err => {
+        .catch((err) => {
           console.log(err);
         });
     });
@@ -451,13 +512,15 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
     const encomienda = {
       numero: r.cod_seguimiento,
       contacto: r.nombre_destinatario,
-      direccion: `${r.direccion_destinatario}, ${this.comunas.find((valor: any) => valor.id === r.id_com_dest).name}`,
+      direccion: `${r.direccion_destinatario}, ${
+        this.comunas.find((valor: any) => valor.id === r.id_com_dest).name
+      }`,
       telefono: r.telefono_destinatario,
       alto: r.alto,
       ancho: r.ancho,
       largo: r.largo,
       peso: r.peso,
-      valor: r.valor_total
+      valor: r.valor_total,
     };
     this.encomiendas.push(encomienda);
 
@@ -468,10 +531,12 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
           nombre: this.selecShop.NombreTienda,
           direccion: `${this.selecShop.direccion}, ${this.selecShop.comuna.name}`,
           telefono: this.selecShop.telefono,
-          referencia: `${this.selecShop.NombreTienda} - ${this.selecShop.referencia} `
+          referencia: `${this.selecShop.NombreTienda} - ${this.selecShop.referencia} `,
         },
         encomiendas: this.encomiendas,
-        valorTotal: this.encomiendas.map(m => m.valor).reduce((a, b) => a + b, 0)
+        valorTotal: this.encomiendas
+          .map((m) => m.valor)
+          .reduce((a, b) => a + b, 0),
       };
       this.resumenUsuario(despachos);
     } else {
@@ -485,17 +550,18 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
     if (this.haveEpecial) {
       Swal.fire({
         title: 'Tenemos un problema',
-        html: 'La comuna ingresada no esta habilitada para recibir desapachos</br> <strong>Ingrese nuevamente la dirección</strong>',
+        html:
+          'La comuna ingresada no esta habilitada para recibir desapachos</br> <strong>Ingrese nuevamente la dirección</strong>',
         icon: 'warning',
         confirmButtonText: 'OK!',
-        heightAuto: false
-      }).then(t => {
+        heightAuto: false,
+      }).then((t) => {
         const modalFiltro = this.dialog.open(ResumenEncomiendaComponent, {
           data: despachos,
           width: '1000px',
-          panelClass: ['filtro-container', 'filtro-content']
+          panelClass: ['filtro-container', 'filtro-content'],
         });
-        modalFiltro.afterClosed().subscribe(e => {
+        modalFiltro.afterClosed().subscribe((e) => {
           // location.reload();
           //   if (e.resp === 'cancelar') {
           //     this.ptoRetiroControl.setValue(this.ptoRetiro[0].id);
@@ -520,9 +586,9 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
       const modalFiltro = this.dialog.open(ResumenEncomiendaComponent, {
         data: despachos,
         width: '1000px',
-        panelClass: ['filtro-container', 'filtro-content']
+        panelClass: ['filtro-container', 'filtro-content'],
       });
-      modalFiltro.afterClosed().subscribe(e => {
+      modalFiltro.afterClosed().subscribe((e) => {
         // location.reload();
         //   if (e.resp === 'cancelar') {
         //     this.ptoRetiroControl.setValue(this.ptoRetiro[0].id);
@@ -543,9 +609,7 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
         //   }
       });
     }
-
   }
-
 
   /** Whether the number of selected elements matches the total number of rows. */
   isAllSelected() {
@@ -556,9 +620,9 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
 
   /** Selects all rows if they are not all selected; otherwise clear selection. */
   masterToggle() {
-    this.isAllSelected() ?
-      this.selection.clear() :
-      this.dataSource.data.forEach(row => this.selection.select(row));
+    this.isAllSelected()
+      ? this.selection.clear()
+      : this.dataSource.data.forEach((row) => this.selection.select(row));
   }
 
   /** The label for the checkbox on the passed row */
@@ -566,7 +630,8 @@ export class FormularioEncomiendaMasivoComponent implements OnInit {
     if (!row) {
       return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
     }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${
+      row.position + 1
+    }`;
   }
-
 }
